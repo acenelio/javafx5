e79765924d7a3833876a724d5f78b0db3c71fa19
Nelio Alves <acenelio@gmail.com>
2018-12-25 17:57:45 -0200

    Adding != null test

--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -6,7 +6,7 @@ public class Constraints {
 
 	public static void setTextFieldInteger(TextField txt) {
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-	        if (!newValue.matches("\\d*")) {
+	        if (newValue != null && !newValue.matches("\\d*")) {
 	        	txt.setText(oldValue);
 	        }
 	    });
@@ -14,7 +14,7 @@ public class Constraints {
 
 	public static void setTextFieldMaxLength(TextField txt, int max) {
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-	        if (newValue.length() > max) {
+	        if (newValue != null && newValue.length() > max) {
 	        	txt.setText(oldValue);
 	        }
 	    });
@@ -22,7 +22,7 @@ public class Constraints {
 
 	public static void setTextFieldDouble(TextField txt) {
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-		    	if (!newValue.matches("\\d*([\\.]\\d*)?")) {
+		    	if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
                     txt.setText(oldValue);
                 }
 		    });
